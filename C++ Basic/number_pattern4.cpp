a403e6d23b64f48b42437acd4f0bf398145ec2e7
Rahul Goswami <121218958+Goswami2001@users.noreply.github.com>
2023-03-02 01:52:53 +0530

    Add files via upload

---
+++ C++ Basic/number_pattern4.cpp
@@ -0,0 +1,20 @@
+// Write a program in c++ to generate following pattern for given rows and cols[Here it's 5] :
+//                                                        1  
+//                                                        1 2 
+//                                                        1 2 3 
+//                                                        1 2 3 4  
+//                                                        1 2 3 4 5 
+#include<iostream>
+using namespace std;
+int main(){
+    int rows;
+    cout<<"Enter the no. of rows : ";
+    cin>>rows;
+    for(int i = 1;i <= rows;i++){
+        for(int j = 1;j <= i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
