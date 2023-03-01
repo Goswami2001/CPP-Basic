b15429d66bcfb061d66122b6a3f176266f18c1c9
Rahul Goswami <121218958+Goswami2001@users.noreply.github.com>
2023-03-01 17:12:16 +0530

    Add files via upload

---
+++ C++ Basic/find_natural_num.cpp
@@ -0,0 +1,10 @@
+//Write a program in C++ to find the first 10 natural numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 10;
+    for(int i = 1;i <= 10;i++){
+        cout<<i<<" ";
+    }
+    return 0;
+}
