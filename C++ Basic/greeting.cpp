20b980d1e640b87ce3ce80fa6570089a6504b78d
Rahul Goswami <121218958+Goswami2001@users.noreply.github.com>
2023-03-05 12:31:06 +0530

    Add files via upload

---
+++ C++ Basic/greeting.cpp
@@ -0,0 +1,11 @@
+//Write a c++ program to greet the user
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    char name [20];
+    cout<<"What's your name ? : ";
+    cin>>name;
+    cout<<"GOOD MONING "<<name<<" have a great day!!";
+    return 0;
+}
