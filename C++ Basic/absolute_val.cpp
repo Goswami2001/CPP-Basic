90eda139c59db36259fdeae4fbf061569ebceb85
Rahul Goswami <121218958+Goswami2001@users.noreply.github.com>
2023-03-09 23:38:16 +0530

    Add files via upload

---
+++ C++ Basic/absolute_val.cpp
@@ -0,0 +1,10 @@
+//Write a c++ program to find out absolute value of a number
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the first number : ";
+    cin>>num;
+    cout<<"Absolute value of "<<num<<" will be : "<<abs(num)<<endl;
+    return 0;
+}
